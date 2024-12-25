4fb5f21b0186cd0b41ed51fb0d4d3826b046b324
AswiniNLP <132350955+AswiniNLP@users.noreply.github.com>
2024-12-25 14:32:59 +0000

    Deploying to gh-pages from @ AswiniNLP/AswiniNLP.github.io@586031c10117e4a16184c73309b958ca74849eb7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,14 +492,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%73%77%69%6E%69%6B%75%6D%61%72%70%61%64%68%69%31%39%39%35@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/https://github.com/AswiniNLP", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/aswini-kumar-08925572/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,14 +520,21 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=https://scholar.google.com/citations?user=SNF6BsQAAAAJ&hl=en", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/https://x.com/aswiniandpunter", "_blank");
+        },
+      },{
+        id: 'social-youtube',
+        title: 'YouTube',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://youtube.com/@https://www.youtube.com/@aswinikumar5205", "_blank");
         },
       },{
       id: 'light-theme',
